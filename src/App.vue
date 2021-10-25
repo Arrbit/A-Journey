a6e6f04c6f4da94da275111909941af9491d06dc
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-10-25 16:37:53 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <v-app>
     <v-main>
-      <ilvlhelper/>
+      <v-toolbar
+      dark
+      prominent
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import ilvlhelper from './components/ilvl_helper.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
-    ilvlhelper,
+    HelloWorld,
   },
 
   data: () => ({
