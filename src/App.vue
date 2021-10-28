090c2dbf61fb6e79a013aa797527de66f0e7386f
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-10-28 12:24:26 +0200

    simple setup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <v-app id="app">
-
-       <v-app-bar
+        <v-app-bar
       app
       color="white"
-      light
+      flat
     >
-      <div class="d-flex align-center">
+      <v-container class="py-0 fill-height">
         <v-img
           alt="SDS Logo"
           class="shrink mr-2"
@@ -16,17 +15,68 @@
           width="40"
         />
 
-        SleepingDragons ilvl-helper
 
-      </div>
-
-      <v-spacer></v-spacer>
-
+        <v-btn
+          v-for="hlinks in hlinks"
+          :key="hlinks"
+          text
+          :href="hlinks.href"
+        >
+          {{ hlinks.text }}
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-container>
     </v-app-bar>
 
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="xpacs in xpacs"
+                  :key="xpacs"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{xpacs.text}}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
 
+                <v-divider class="my-2"></v-divider>
+
+                  <v-list-item
+                  v-for="modes in modes"
+                  :key="modes"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                    {{modes.text}}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
 
     <router-view/>
+
+                </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
 
     <v-footer
       padless="padless"
@@ -67,11 +117,31 @@ export default {
   name: 'App',
 
   data: () => ({
+        hlinks: [
+       {text: 'Home', href: 'https://sleepingdragons.eu/' },
+       {text: 'Events', href: 'https://sleepingdragons.eu/upcoming-events/' },
+       {text: 'Guides', href: 'https://sleepingdragons.eu/guides/' },
+       {text: 'Letters', href: 'https://sleepingdragons.eu/letter/' },
+       {text: 'Members', href: 'https://sleepingdragons.eu/members/' },
+       {text: 'Discord', href: 'https://discord.com/invite/4xh39AD' },
+      ],
     flinks: [
        {text: 'Legal Disclosure', href: 'https://sleepingdragons.eu/contact/' },
        {text: 'Privacy Policy', href: 'https://sleepingdragons.eu/privacypolicy/' },
        {text: 'Netlifys GDPR', href: 'https://www.netlify.com/gdpr-ccpa/' },
        {text: 'Contact', href: 'https://sleepingdragons.eu/contact/' },
+      ],
+      xpacs: [
+       {text: 'A Realm Reborn', href: 'https://sleepingdragons.eu/contact/' },
+       {text: 'Heavensward', href: 'https://sleepingdragons.eu/privacypolicy/' },
+       {text: 'Stormblood', href: 'https://www.netlify.com/gdpr-ccpa/' },
+       {text: 'Shadowbringer', href: 'https://sleepingdragons.eu/contact/' },
+      {text: 'Endwalker', href: 'https://sleepingdragons.eu/contact/' },
+      ],
+      modes: [
+       {text: 'Overview', href: 'https://sleepingdragons.eu/contact/' },
+       {text: 'Level Progression', href: 'https://sleepingdragons.eu/privacypolicy/' },
+       {text: 'Patch Progression', href: 'https://www.netlify.com/gdpr-ccpa/' },
       ],
   }),
 };
