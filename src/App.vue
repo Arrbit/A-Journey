972a243992a0a446aa19b03da601bb3e044f8850
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-11-08 17:37:04 +0100

    add icon fontset and final main design

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,81 @@
 <template>
 <v-app id="app">
 
-  <v-app-bar
-  app
-  color="white"
-  flat>
-    <v-container class="py-0 fill-height">
-      <v-btn text href="/">
-        <v-img
-        alt="SDS Logo"
-        class="shrink mr-2"
-        contain
-        src="https://sleepingdragons.eu/wp-content/themes/New-Sleeping-Dragons/assets/logos/VectorRed_Transparent.png"
-        transition="scale-transition"
-        width="40"
-        />
-      </v-btn>
+    <v-app-bar app class="lighten-4 grey black--text" dense>
+        <v-container class="py-0 fill-height">
 
+            <v-spacer />
+            <v-btn v-for="links in hlinks" :key="links.text" text :href="links.href">
+                {{ links.text }}
+                <v-icon v-if="!hlinks.icon"> {{ links.icon }} </v-icon>
+            </v-btn>
+            <v-spacer />
+        </v-container>
+    </v-app-bar>
+    <router-view />
 
-      <v-btn
-      v-for="links in hlinks"
-      :key="links.text"
-      text
-      :href="links.href">
-      {{ links.text }}
-      </v-btn>
-      <v-spacer/>
-    </v-container>
-  </v-app-bar>
+    <v-footer padless>
+        <v-card flat tile width="100%" class="lighten-4 grey text-center black--text">
 
-  <router-view/>
+            <v-btn v-for="links in flinks" :key="links.text" :href="links.href" text small rounded class="my-2">
+                {{ links.text }}
+            </v-btn>
 
-  <v-footer padless>
-    <v-card
-      flat
-      tile
-      width="100%"
-      class="text-center black--text">
+            <v-divider />
 
-      <v-btn
-        v-for="links in flinks"
-        :key="links.text"
-        :href="links.href"
-        text
-        small
-        rounded
-        class="my-2">
-      {{ links.text }}
-      </v-btn>
-
-    <v-divider/>
-
-      <v-card-text class="black--text">
-        Hosted by Netlify | {{ new Date().getFullYear() }} — <strong>SleepingDragons</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+            <v-card-text class="black--text">
+                Hosted by Netlify | {{ new Date().getFullYear() }} — <strong>SleepingDragons</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
 
 </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+    name: 'App',
 
-  data: () => ({
-        hlinks: [
-       {text: 'Home', href: 'https://sleepingdragons.eu/' },
-       {text: 'Events', href: 'https://sleepingdragons.eu/upcoming-events/' },
-       {text: 'Guides', href: 'https://sleepingdragons.eu/guides/' },
-       {text: 'Letters', href: 'https://sleepingdragons.eu/letter/' },
-       {text: 'Members', href: 'https://sleepingdragons.eu/members/' },
-       {text: 'Discord', href: 'https://discord.com/invite/4xh39AD' },
-      ],
-    flinks: [
-       {text: 'Legal Disclosure', href: 'https://sleepingdragons.eu/contact/' },
-       {text: 'Privacy Policy', href: 'https://sleepingdragons.eu/privacypolicy/' },
-       {text: 'Netlifys GDPR', href: 'https://www.netlify.com/gdpr-ccpa/' },
-      ],
-  }),
+    data: () => ({
+        hlinks: [{
+                text: 'Home',
+                href: 'https://sleepingdragons.eu/'
+            },
+            {
+                text: 'Events',
+                href: 'https://sleepingdragons.eu/upcoming-events/'
+            },
+            {
+                text: 'Guides',
+                href: 'https://sleepingdragons.eu/guides/'
+            },
+            {
+                text: 'Letters',
+                href: 'https://sleepingdragons.eu/letter/'
+            },
+            {
+                text: 'Members',
+                href: 'https://sleepingdragons.eu/members/'
+            },
+            {
+                text: 'Discord',
+                href: 'https://discord.com/invite/4xh39AD',
+                icon: 'mdi-discord'
+            },
+        ],
+        flinks: [{
+                text: 'Legal Disclosure',
+                href: 'https://sleepingdragons.eu/contact/'
+            },
+            {
+                text: 'Privacy Policy',
+                href: 'https://sleepingdragons.eu/privacypolicy/'
+            },
+            {
+                text: 'Netlifys GDPR',
+                href: 'https://www.netlify.com/gdpr-ccpa/'
+            },
+        ],
+    }),
 };
 </script>
--- src/components/5EW/overview.vue
+++ src/components/5EW/overview.vue
@@ -1,5 +1,5 @@
-<template>
-   <div>
-    Endwalker overview
-   </div>
+<template>   
+<v-card min-height="70vh" class="pa-2">
+   More Updates coming soon
+</v-card>
 </template>
