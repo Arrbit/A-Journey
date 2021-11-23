f5112f1ea2796e26c78a009de6b29f03447ebab5
asfalfa <glkin9@gmail.com>
2021-11-23 15:51:38 +0100

    ew ilvl and patch redesign

--- src/components/5EW/ilevel.vue
+++ src/components/5EW/ilevel.vue
@@ -7,276 +7,85 @@
             <v-card-title style="justify-content: center;" class="text-caption">
             Endwalker Information is based on past expansions' content cycle and will be updated as information comes out
             </v-card-title>
-              <v-expansion-panels accordion>
-               <v-expansion-panel v-for="level in levels" :key="level">
-                  <v-expansion-panel-header class="text-h6  text-center" @click="reveal = false; reveal2 = false; reveal3 = false; reveal4 = false; reveal5 = false; reveal6 = false;">
-                    {{ level.title }}
-                  </v-expansion-panel-header>
-                  <v-expansion-panel-content>
+              <v-card rounded elevation="0" class="mb-5" v-for="level in endwalker.levels" :key="level">
+                  <v-card-title class="text-h6 font-weight-bold text-center header" style="justify-content: center;">
+                    {{ level.name }}
+                  </v-card-title>
+                  <v-divider />
+                  <v-card-text>
                      <v-row>
-                        <v-col v-for="patch in level.patches" :key="patch">
+                        <v-col v-for="content in level.content" :key="content.ilvl">
                            <v-tooltip bottom>
                               <template v-slot:activator="{ on, attrs }">
-                                 <v-card elevation="0" class="pb-5" style="display: grid; justify-content: center;" v-bind="attrs" v-on="on">
-                                    <v-card-title>Patch {{ patch.number }}</v-card-title>
+                                 <v-card rounded elevation="0" class="pb-5" style="display: grid; justify-content: center;" v-bind="attrs" v-on="on">
+                                  <v-card-title>Patch {{ content.patch }}</v-card-title>
                                     <div class="d-flex flex-row" style="justify-content: center;">
-                                       <div v-for="icon in patch.icons" :key="icon">
+                                       <div v-for="type in content.type" :key="type">
                                        <v-img
                                           contain
                                           style= "max-width: max-content; justify-self: center;"
-                                          :src="icon.icon"
+                                          :src="type.icon"
                                        ></v-img>
                                        </div>
                                     </div>
                                  </v-card>
                               </template>
-                              <v-card-text class="text-body-1">
-                                 {{ patch.text }}
+                              <v-card-text v-for="type in content.type" :key="type" class="text-body-1">
+                                 {{ type.text }}
+                                 <v-card-text v-if="type.weekly!=false || type.gearType!=null || type.midPatch!=null">
+                                  <v-row>
+                                    <template v-if="type.weekly!=false">Weekly</template>
+                                    <template v-if="type.weekly!=false && type.gearType!=null || type.weekly!=false && type.midPatch!=null"><v-divider vertical class="mx-3"></v-divider></template>
+                                    <template v-if="type.midPatch!=null">Patch {{ type.midPatch }}</template>
+                                    <template v-if="type.gearType!=null && type.midPatch!=null"><v-divider vertical class="mx-3"></v-divider></template> 
+                                    <template v-if="type.gearType!=null">{{ type.gearType }}</template>
+                                    <template v-if="type.gearType!=null && type.midIlvl!=null"><v-divider vertical class="mx-3"></v-divider></template>
+                                    <template v-if="type.midIlvl!=null">iLvl {{ type.midIlvl }}</template>
+                                  </v-row>
+                                 </v-card-text>
                               </v-card-text>
                            </v-tooltip>
                         </v-col>
                      </v-row>
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-              </v-expansion-panels>
+                  </v-card-text>
+              </v-card>
             </v-sheet>
 </template>
 <style scoped>
-.v-expansion-panel-header--active{
-  color: #0091ea;
+.header{
+  color: #0091ea !important;
 }
-.v-expansion-panel--active{
-  background-color: #e8e8e8 !important;
+
+.v-card__title{
+  color: #3c3c3c;
 }
 
 .v-card{
   background-color: #e8e8e8;
 }
 
+.v-card:hover{
+  background-color: #dbdbdb;
+}
+
 .v-card__text{
     white-space: pre;
+}
+
+.v-divider{
+  margin-right: 500px;
+  margin-left: 500px;
 }
 </style>
 
 
 <script>
-const crafted = require('../../assets/crafted.png')
-const dungeon = require('../../assets/dungeon.png')
-const quest = require('../../assets/quest.png')
-const raid = require('../../assets/raid.png')
-const savage = require('../../assets/savage.png')
-const tomestone = require('../../assets/tomestone.png')
-const trial = require('../../assets/trial.png')
+import expacs from '../../data.js';
 
   export default {
     data: () => {
       return {
-       levels: [
-            {
-            title: '560',
-            patches: [
-               { number: '6.0', 
-                 icons: [
-                    { icon: quest }, 
-                    { icon: dungeon }, 
-                  ],
-                  text: `Quest Gear\nDungeon Gear`
-               },
-               ]
-            },
-            {
-            title: '570',
-            patches: [
-               { number: '6.0', 
-                 icons: [
-                    { icon: tomestone }, 
-                  ],
-                  text: `Tomestone Gear`
-               },
-               { number: '6.1',
-                  icons: [
-                    { icon: dungeon },
-                  ],
-                  text: `Dungeon Gear` 
-               },
-               ]
-            },
-            {
-            title: '580',
-            patches: [
-               { number: '6.0',
-                  icons: [
-                    { icon: trial },
-                  ],
-                  text: `EX Trial (Weapon + Accessories)`
-               },
-               { number: '6.01',
-                  icons: [
-                    { icon: raid },
-                  ],
-                  text: `Normal Raid 1-4 (Weekly) `
-               },
-               { number: '6.05',
-                  icons: [
-                    { icon: crafted },
-                  ],
-                  text: `Crafted Gear`
-               },
-               { number: '6.2',
-                  icons: [
-                    { icon: dungeon },
-                  ],
-                  text: `Dungeon Gear`
-               },
-               ]
-            },
-            {
-            title: '590',
-            patches: [
-               { number: '6.05', 
-                 icons: [
-                    { icon: tomestone }, 
-                  ],
-                  text: `Tomestone Gear (Weekly)`
-               },
-               { number: '6.1',
-                  icons: [
-                    { icon: raid },
-                    { icon: trial },
-                  ],
-                  text: `24-man Normal No.1 (Weekly)\nEX Trial (Weapon) (iLvl 595)` 
-               },
-               { number: '6.2',
-                  icons: [
-                    { icon: tomestone },
-                  ],
-                  text: `Tomestone Gear (Unlimited)`
-               },
-               ]
-            },
-            {
-            title: '600',
-            patches: [
-               { number: '6.05',
-                  icons: [
-                    { icon: tomestone },
-                    { icon: savage },
-                  ],
-                  text: `Upgraded Tomestone Gear\nSavage Raid Gear\nSavage Raid Weapon (iLvl 605)`
-               },
-               { number: '6.3',
-                  icons: [
-                    { icon: dungeon },
-                  ],
-                  text: `Dungeon Gear`
-               },
-               ]
-            },
-            {
-            title: '610',
-            patches: [
-               { number: '6.2', 
-                 icons: [
-                    { icon: raid }, 
-                    { icon: crafted }, 
-                    { icon: trial }, 
-                  ],
-                  text: `Normal Raid 5-8 (Weekly)\nCrafted Gear\nEX Trial (Weapon) (iLvl 615)`
-               },
-               { number: '6.4',
-                  icons: [
-                    { icon: dungeon },
-                  ],
-                  text: `Dungeon Gear` 
-               },
-               ]
-            },
-            {
-            title: '620',
-            patches: [
-               { number: '6.2', 
-                 icons: [
-                    { icon: tomestone }, 
-                  ],
-                  text: `Tomestone Gear (Weekly)`
-               },
-               { number: '6.3',
-                  icons: [
-                    { icon: raid },
-                    { icon: trial },
-                  ],
-                  text: `24-man Normal No.2 (Weekly)\nEX Trial (Weapon) (iLvl 625)` 
-               },
-               { number: '6.4',
-                  icons: [
-                    { icon: tomestone },
-                  ],
-                  text: `Tomestone Gear (Unlimited)`
-               },
-               ]
-            },
-            {
-            title: '630',
-            patches: [
-               { number: '6.2',
-                  icons: [
-                    { icon: tomestone },
-                    { icon: savage },
-                  ],
-                  text: `Upgraded Tomestone Gear\nSavage Raid Gear\nSavage Raid Weapon (iLvl 635)`
-               },
-               { number: '6.5',
-                  icons: [
-                    { icon: dungeon },
-                  ],
-                  text: `Dungeon Gear`
-               },
-               ]
-            },
-            {
-            title: '640',
-            patches: [
-               { number: '6.4', 
-                 icons: [
-                    { icon: raid }, 
-                    { icon: crafted }, 
-                    { icon: trial }, 
-                  ],
-                  text: `Normal Raid 9-12 (Weekly)\nCrafted Gear\nEX Trial (Weapon) (iLvl 645)`
-               },
-               ]
-            },
-            {
-            title: '650',
-            patches: [
-               { number: '6.4', 
-                 icons: [
-                    { icon: tomestone }, 
-                  ],
-                  text: `Tomestone Gear (Weekly)`
-               },
-               { number: '6.5',
-                  icons: [
-                    { icon: raid },
-                    { icon: trial },
-                  ],
-                  text: `24-man Normal No.3 (Weekly)\nEX Trial (Weapon) (iLvl 655)` 
-               },
-               ]
-            },
-            {
-            title: '660',
-            patches: [
-               { number: '6.4',
-                  icons: [
-                    { icon: tomestone },
-                    { icon: savage },
-                  ],
-                  text: `Upgraded Tomestone Gear\nSavage Raid Gear\nSavage Raid Weapon (iLvl 665)`
-               },
-               ]
-            },
-      ],
+          endwalker: expacs.endwalker
     }
   }
 }
